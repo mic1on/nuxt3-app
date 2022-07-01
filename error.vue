9f9554545a0db76f3c65d46f6d8e08d7d692f071
miclon <jcnd@163.com>
2022-07-01 10:29:13 +0800

    feat: error page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <n-result status="500" title="异常出错" :description="error.message">
+    <template #footer>
+      <n-button>回到首页</n-button>
+    </template>
+  </n-result>
+  </div>
+</template>
+<script setup lang="ts">
+import { NResult, NButton } from 'naive-ui';
+
+const error = useError()
+
+</script>
